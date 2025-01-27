40c4b8dafb7d016861cd1c94211b1a895b67253a
priyangshu24 <138028617+priyangshu24@users.noreply.github.com>
2025-01-28 00:55:21 +0530

    fills added

--- src/app/component/Navbar.tsx
+++ src/app/component/Navbar.tsx
@@ -24,21 +24,48 @@ const Navbar = () => {
                                 variant:'ghost'})}>
                                     Sign Out
                                 </Link>
-                            {isAdmin ? <Link href='/api/auth/logout' className={buttonVariants({
-                                size :'sm', 
-                                variant:'ghost'})}>
-                                    Dashboard ✨
-                                </Link> : null}
-                                </Link>
+                                {isAdmin ? (
+                                    <Link 
+                                    href='/api/auth/logout'
+                                    className={buttonVariants({
+                                        size :'sm',
+                                        variant : 'ghost',
+                                    })}>
+                                        Dashboard ✨
+                                    </Link>
+                                ): null}
+                          
                             <Link href='/configure/upload' className={buttonVariants({
                                 size :'sm', 
                                 className:'hidden sm:flex item-center gap-1'})}>
                                     Create case 
-                                    <ArrowRight className="ml-2 h-4 w-4" />
+                                    <ArrowRight className="ml-1.5 h-5 w-5"/>
                                 </Link> 
                         </>
                     ):(
-                        <></>
+                        <>
+                        <Link href='/api/auth/register' 
+                        className={buttonVariants({
+                            size :'sm', 
+                            variant:'ghost'})}>
+                                Sign up
+                            </Link>
+                        <Link href='/api/auth/login' 
+                        className={buttonVariants ({
+                            size :'sm', 
+                            variant:'ghost'})}>
+                                login
+                            </Link>  
+    
+                                <div className='h-8 w-px bg-zinc-200 hidden sm:block'/>
+                                <Link href='/configure/upload' 
+                                className={buttonVariants({
+                                size :'sm', 
+                                className:'hidden sm:flex item-center gap-1'})}>
+                                    Create case 
+                                    <ArrowRight className="ml-1.5 h-5 w-5"/>
+                                </Link>
+                            </>
                     )}
                 </div>
             </div>
