994063f9ec7b4fa7e1d7a59f48ca231c76256980
priyangshu24 <138028617+priyangshu24@users.noreply.github.com>
2025-02-06 03:24:07 +0530

    fills added

--- src/app/configure/upload/page.tsx
+++ src/app/configure/upload/page.tsx
@@ -9,13 +9,36 @@ import Dropzone, {FileRejection} from "react-dropzone";
 const Page = () => {
 
     const [isDragOver, setIsDragOver] = useState<boolean>(false)
+
+    const onDropRejected = () => {}
+    const onDropAccepted = () => {}
+
     return <div className={cn('relative h-full flex-1 my-16 w-full roinded-xl bg-gray-900/5 p-2 ring-1 ring-inset ring-gray-900/10 lg:rounded-2xl flex justify-center flex-col items-center',
         {
           'ring-blue-900/25 bg-blue-900/10': isDragOver,
         }
     )}>
         <div className="relative flex flex-l flex-col items-center justify-center w-full">
-            <Dropzone></Dropzone>
+            <Dropzone 
+                onDropRejected={onDropRejected}
+                onDropAccepted={onDropAccepted}
+                accept={{'image/png': [], 
+                         'image/jpeg': [], 
+                         'image/jpg': [], 
+                }}
+                onDragEnter={() => setIsDragOver(true)}
+                onDragLeave={() => setIsDragOver(false)}
+                >   
+                {({getRootProps , getInputProps}) => (
+                    <div className="h-full w-full flex flex-col items-center justify-center" {...getRootProps()}>
+                        <input {...getInputProps()} />
+                    </div>
+
+                )}
+                
+            </Dropzone>
         </div>
     </div>
 };
+
+export default Page
