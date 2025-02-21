e78d11424e656f889f427de7100addd2d7f651d2
priyangshu24 <138028617+priyangshu24@users.noreply.github.com>
2025-02-22 03:39:33 +0530

    fills added

---
+++ src/app/configure/design/DesignConfigurator.tsx
@@ -0,0 +1,35 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+import { AspectRatio } from "@radix-ui/react-aspect-ratio";
+import NextImage from "next/image";
+
+interface DesignConfiguratorProps {
+    configId: string;
+    imageUrl: string;
+    imageDimensions: {
+        width: number;
+        height: number;
+    }
+}
+const DesignConfigurator = ({
+    configId, 
+    imageUrl, 
+    imageDimensions
+}: DesignConfiguratorProps) => {
+    return <div className="relative mt-20 grid grid-cols-3 mb-20 pb-20">
+        <div className="relative h-[37.5rem] overflow-hidden col-span-2 w-full max-w-4xl flex items-center justyfy-center rounded-lg border-2 border-dashed border-gray-300 p-12 text-center focus:outline-none float-right-2 focus:focus-primary focus:ring-offset-2">
+            <div className="relative w-60 bg-opacity-50 pointer-events-none aspect-[896/1831]">
+            <AspectRatio ratio={896/1831} className="pointer-events-none
+            relative z-50 aspect-[896/1831]">
+                <NextImage 
+                fill
+                alt="phone image " 
+                src="/phone-template.png"
+                className="pointer-events-none z-50 select-none"/>
+            </AspectRatio> 
+            </div>
+        </div>
+    </div>
+
+}
+
+export default DesignConfigurator;
--- src/app/configure/design/page.tsx
+++ src/app/configure/design/page.tsx
@@ -1,3 +1,7 @@
+import { db } from "@/db";
+import { notFound } from "next/navigation";
+import DesignConfigurator from "./DesignConfigurator";
+
 interface pageProps {
     searchParams: {
         [key: string]: string | string[] | undefined
@@ -6,8 +10,22 @@ interface pageProps {
 }
 const page = async ({ searchParams }: pageProps) => {
     const { id } = searchParams
-    // make db call
-    return <p>{id}</p>
+
+    if (!id || typeof id !== 'string') {
+        return notFound()
+    }
+    const configuration = await db.configuration.findUnique({
+        where: { id },
+    }) 
+    if (!configuration) {
+        return notFound()
+    }
+    const { imageUrl, height, width } = configuration
+    return < DesignConfigurator 
+    configId={configuration.id}
+    imageDimensions = {{width, height}} 
+    imageUrl={imageUrl}
+    />
 }
 
 export default page
