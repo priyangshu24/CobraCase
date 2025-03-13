74e81ab89ccd6c90bd33814669c2efaba94785a5
priyangshu24 <138028617+priyangshu24@users.noreply.github.com>
2025-03-14 00:05:22 +0530

    fills added

--- src/app/configure/design/page.tsx
+++ src/app/configure/design/page.tsx
@@ -1,31 +1,39 @@
-import { db } from "@/db";
-import { notFound } from "next/navigation";
-import DesignConfigurator from "./DesignConfigurator";
+import { db } from "@/db"
+import { notFound } from "next/navigation"
+import DesignConfigurator from "./DesignConfigurator"
 
-interface pageProps {
-    searchParams: {
-        [key: string]: string | string[] | undefined
-    }
-    
+interface SearchParams {
+  [key: string]: string | string[] | undefined
 }
-const page = async ({ searchParams }: pageProps) => {
-    const { id } = searchParams
 
-    if (!id || typeof id !== 'string') {
-        return notFound()
-    }
-    const configuration = await db.configuration.findUnique({
-        where: { id },
-    }) 
-    if (!configuration) {
-        return notFound()
-    }
-    const { imageUrl, height, width } = configuration
-    return < DesignConfigurator 
-    configId={configuration.id}
-    imageDimensions = {{width, height}} 
-    imageUrl={imageUrl}
+interface PageProps {
+  searchParams: SearchParams
+}
+
+const Page = async ({ searchParams }: PageProps) => {
+  const { id } = searchParams
+
+  if (!id || typeof id !== 'string') {
+    return notFound()
+  }
+
+  const configuration = await db.configuration.findUnique({
+    where: { id },
+  })
+
+  if (!configuration) {
+    return notFound()
+  }
+
+  const { imageUrl, height, width } = configuration
+
+  return (
+    <DesignConfigurator 
+      configId={configuration.id}
+      imageDimensions={{ width, height }}
+      imageUrl={imageUrl}
     />
+  )
 }
 
-export default page
+export default Page
